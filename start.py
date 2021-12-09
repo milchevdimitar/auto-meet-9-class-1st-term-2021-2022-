5119236982d2d4c7ba0cd119d750ae31382aab8f
milchevdimitar <85071480+milchevdimitar@users.noreply.github.com>
2021-12-09 22:52:44 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,12 +1,16 @@
+#codes/custom-meet-scr/start.py
+#изпробвано на 1920х1080
+#ако имате 2 монитора или повече - ще го изпълни на най - левия
 import pyautogui
 import webbrowser
 import time
 
-pyautogui.moveTo(771, 767, 4)
-pyautogui.click()
+def click():
+    pyautogui.moveTo(771, 767, 2)
+    pyautogui.click()
+    pyautogui.moveTo(688, 785, 0.5)
+    pyautogui.click()
+    pyautogui.moveTo(1281, 641, 2)
+    pyautogui.click()
 
-pyautogui.moveTo(688, 785, 4)
-pyautogui.click()
-
-pyautogui.moveTo(1281, 641, 4)
-pyautogui.click()
+click()
