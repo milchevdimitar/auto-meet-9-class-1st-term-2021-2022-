f5b7ee6eec477c4d2919cfaa43f4c56bed18d0b0
milchevdimitar <85071480+milchevdimitar@users.noreply.github.com>
2021-12-09 22:27:24 +0200

    Add files via upload

---
+++ start.py
@@ -0,0 +1,12 @@
+import pyautogui
+import webbrowser
+import time
+
+pyautogui.moveTo(771, 767, 4)
+pyautogui.click()
+
+pyautogui.moveTo(688, 785, 4)
+pyautogui.click()
+
+pyautogui.moveTo(1281, 641, 4)
+pyautogui.click()
